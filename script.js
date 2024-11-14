6bc1c621e871a5ae799d2dd3da92f3a053e763dc
andpasant <andpasant@gmail.com>
2024-11-14 23:06:13 +0100

    Initial commit: Dateien hinzugefügt

---
+++ script.js
@@ -0,0 +1,124 @@
+
+// Access HTML elements
+let doorImage1 = document.getElementById('door1')
+let doorImage2 = document.getElementById('door2')
+let doorImage3 = document.getElementById('door3')
+
+let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg';
+let beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg';
+let closedDoorPath = 'https://content.codecademy.com/projects/chore-door/images/closed_door.svg';
+
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+let currentlyPlaying = true;
+
+let startButton = document.getElementById('start')
+
+// Define game logic to check doors, progress game, end game, and choose a random chore door
+
+// Function to check if the door is clicked and still closed
+function isClicked(door){
+  // Check if the door's src is equal to closedDoorPath
+  if(door.src===closedDoorPath){
+    return true; // Door is closed
+	console.log('true')
+  } else{
+    return false; // Door is opened
+  }
+}
+
+function isBot(door){
+  // Check if the door's src is equal to closedDoorPath
+  if(door.src===botDoorPath){
+    return true; // Door is closed
+  } 
+    return false; // Door is opened
+  } 
+
+
+function gameOver(status){
+  if(status==='win'){
+    startButton.innerHTML='You win! Play again?'; 
+  } else{
+    startButton.innerHTML='Game over! Play again';
+  } 
+  currentlyPlaying=false;
+}
+
+function playDoor(door){
+  numClosedDoors-=1;
+  if (numClosedDoors==0){
+    gameOver('win')
+  }
+  else if (isBot(door)){
+    gameOver('lose')
+  }
+}
+
+function randomChoreDoorGenerator(){
+  let choreDoor= Math.floor(Math.random() * 3);
+  if (choreDoor===0){
+    openDoor1=botDoorPath;
+    openDoor2=beachDoorPath;
+    openDoor3=spaceDoorPath;
+  }
+    else if (choreDoor===1){
+      openDoor1=beachDoorPath;
+      openDoor2=botDoorPath;
+      openDoor3=spaceDoorPath;
+    } else {
+        openDoor1=beachDoorPath;
+        openDoor2=spaceDoorPath;
+        openDoor3=botDoorPath;
+		}
+}
+
+
+
+
+doorImage1.onclick = () => {
+  if (currentlyPlaying && isClicked(doorImage1)) {
+    doorImage1.src = openDoor1;
+    playDoor(doorImage1);
+  }
+}
+
+doorImage2.onclick = () => {
+  if (currentlyPlaying && isClicked(doorImage2)) {
+    doorImage2.src = openDoor2;
+    playDoor(doorImage2);
+  }
+}
+
+doorImage3.onclick = () => {
+  if (currentlyPlaying && isClicked(doorImage3)) {
+    doorImage3.src = openDoor3;
+    playDoor(doorImage3);
+  }
+}
+
+startButton.onclick = () => {
+  if (currentlyPlaying === false) {
+    startRound();
+  }
+}
+
+// Start a game round
+function startRound(){
+  doorImage1.src=closedDoorPath;
+  doorImage2.src=closedDoorPath;
+  doorImage3.src=closedDoorPath;
+  numClosedDoors = 3;
+  currentlyPlaying = true;
+ 	startButton.innerHTML='Good Luck!';
+  randomChoreDoorGenerator()
+
+}
+
+startRound();
+
+
+
